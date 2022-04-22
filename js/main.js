dd0e3197958ca5e3ef772308fd7749361694f24c
HapaxCode <dafunkyhapax@protonmail.com>
2022-04-22 18:12:01 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let referralCodes = ['STAR-VXJR-7MLS', 'STAR-H9QF-FB34', 'STAR-L4B4-VB6Z']
+const randomize = document.querySelector('.randomize');
+
+function randomReferralFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
+}
+
+randomize.addEventListener('click', result);
+
+let generator = document.querySelector('#yourReferral');
+
+function result() {
+    document.querySelector('#result').style.height = '164px';
+    document.querySelector('#yourReferral').style.height = '110px';
+    setTimeout(resultDisplay, 800);
+}
+
+function resultDisplay() {
+    const referral = randomReferralFromArray(referralCodes);
+    generator.textContent = referral;
+    document.querySelector('#yourReferralComment').style.display = 'block';
+}
