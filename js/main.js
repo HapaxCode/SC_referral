bf47fcfbf5c265cb938de56e65c07ca268bd3302
HapaxCode <dafunkyhapax@protonmail.com>
2022-04-23 10:29:20 +0200

    ajout du nom du citoyen, passage en responsive, et copie au clic sur le referral

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let referralCodes = ['STAR-VXJR-7MLS', 'STAR-H9QF-FB34', 'STAR-L4B4-VB6Z', 'STAR-ZSMG-R9P9', 'STAR-GY4M-S664', 'STAR-DVV4-NM9S']
+let referralCodes = [['STAR-VXJR-7MLS', 'Xaxa84'], ['STAR-H9QF-FB34','DafunkyHapax'], ['STAR-L4B4-VB6Z', 'Nico_Farseeker'], ['STAR-ZSMG-R9P9', 'Mr_Teigneux'], ['STAR-GY4M-S664', 'DK-Asabrag'], ['STAR-DVV4-NM9S', 'Kryhos'], ['STAR-RKRT-NCSR', 'So666'], ['STAR-VP3Q-C6RQ','Shrek']]
 const randomize = document.querySelector('.randomize');
 
 function randomReferralFromArray(array){
@@ -6,18 +6,34 @@ function randomReferralFromArray(array){
     return array[random];
 }
 
-randomize.addEventListener('click', result);
-
-let generator = document.querySelector('#yourReferral');
-
-function result() {
-    document.querySelector('#result').style.height = '164px';
-    document.querySelector('#yourReferral').style.height = '110px';
-    setTimeout(resultDisplay, 800);
-}
+randomize.addEventListener('click', resultDisplay);
 
 function resultDisplay() {
-    const referral = randomReferralFromArray(referralCodes);
+    document.getElementById('result').style.height = '164px';
+    document.getElementById('yourReferral').style.height = '110px';
+    setTimeout(finalResult, 800);
+}
+
+function finalResult() {
+    const referralAndName = randomReferralFromArray(referralCodes);
+    const referral = referralAndName[0];
+    const name = referralAndName[1];
+    let generator = document.getElementById('yourReferral');
     generator.textContent = referral;
-    document.querySelector('#yourReferralComment').style.display = 'block';
+    let referralName = document.getElementById('yourReferralNameSpan');
+    referralName.textContent = name;
+    document.getElementById('yourReferralComment').style.display = 'block';
+    document.getElementById('yourReferralName').style.display = 'block';
+}
+
+function copy_to_clipboard(yourReferral) {
+    var text = document.getElementById(yourReferral).innerHTML;
+    navigator.clipboard.writeText(text).then(function() {
+        /* clipboard successfully set */
+        // on change l'apparence du texte pour montrer qu'il a été copié :
+        document.getElementById(yourReferral).style.color = "grey"; 
+        document.getElementById(yourReferral).style.fontStyle = "italic";
+    }, function() {
+        /* clipboard write failed */
+    });
 }
